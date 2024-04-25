823f90f5e916358ede032ef5b704f5c062d52301
Hui Zhou <hui.zhou@yum.com>
2024-04-24 22:54:17 -0500

    Add Mocha test sample using axios to communicate with Report Portal

---
+++ 2.axios/test/axios.spec.js
@@ -0,0 +1,135 @@
+var assert = require("assert");
+const axios = require("axios");
+
+const REPORTPORTAL_URL = "http://localhost:8080";
+const REPORTPORTAL_API_KEY =
+  "{{the report portal api key}}";
+const REPORTPORTAL_PROJECT = "poc";
+
+const axioInstance = axios.create({
+  baseURL: REPORTPORTAL_URL,
+  timeout: 10000,
+  headers: {
+    Authorization: `Bearer ${REPORTPORTAL_API_KEY}`,
+    "Content-Type": "application/json",
+  },
+});
+
+describe("HTTP query parameter", function () {
+  let launchId = "";
+  before(async function () {
+    // Start ReportPortal Launch
+    try {
+      const { data } = await axioInstance.post(
+        `/api/v1/${REPORTPORTAL_PROJECT}/launch`,
+        {
+          name: "REST Test",
+          description: "Test Report Portal REST API start\n",
+          startTime: new Date().toISOString(),
+          mode: "DEFAULT",
+          attributes: [
+            {
+              key: "version",
+              value: "0.0.1",
+            },
+            {
+              value: "test",
+            },
+          ],
+        }
+      );
+      launchId = data.id;
+      console.log(`## Start launch: ${JSON.stringify(data)}`);
+    } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    }
+  });
+  it("should be 1", async function () {
+    const { data } = await axioInstance.post(
+      `/api/v1/${REPORTPORTAL_PROJECT}/item`,
+      {
+        name: "Start suite item",
+        description: "Start root item\n",
+        startTime: new Date().toISOString(),
+        type: "suite",
+        launchUuid: launchId,
+      }
+    );
+    const rootItemId = data.id;
+
+    await axioInstance.post(`/api/v1/${REPORTPORTAL_PROJECT}/log`, {
+      launchUuid: launchId,
+      itemUuid: rootItemId,
+      time: Date.now(),
+      level: 10000,
+      message: `Request:
+url: "https://httpbin.org/get?q1=1"`,
+    });
+    const res = await axios.get("https://httpbin.org/get?q1=1");
+    await axioInstance.post(`/api/v1/${REPORTPORTAL_PROJECT}/log`, {
+      launchUuid: launchId,
+      itemUuid: rootItemId,
+      time: Date.now(),
+      level: 10000,
+      message: `Response:${JSON.stringify(res.data, null, 2)}`,
+    });
+    assert.equal(res.data.args.q1, 1, "The expected query parameter is not 1");
+
+    await axioInstance.put(
+      `/api/v1/${REPORTPORTAL_PROJECT}/item/${rootItemId}`,
+      {
+        name: "Finish suite item",
+        description: "Finish root item\n",
+        endTime: new Date().toISOString(),
+        status: "passed",
+      }
+    );
+  });
+
+  after(async function () {
+    // Finish ReportPortal Launch
+    try {
+      const { data } = await axioInstance.put(
+        `/api/v1/${REPORTPORTAL_PROJECT}/launch/${launchId}/finish`,
+        {
+          description: "Test Report Portal REST API finish",
+          endTime: new Date().toISOString(),
+          status: "passed",
+        }
+      );
+      console.log(`## Launch finish: ${JSON.stringify(data)}`);
+    } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    }
+  });
+});
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ This project demonstrates the [MochaJS](https://mochajs.org/) and [Report Portal
 ### 1.basic
 
 The fundamental Mocha test sample integrated with Report Portal. It shows the minimum dependency. The test also shows how to utilize the Report Portal REST API. It helps to understand the basic report portal concept.
+
+### 2.axios
+
+Similar to the basic sample. It replaces the NodeJS built in _fetch_ API with _axios_ dependency.
